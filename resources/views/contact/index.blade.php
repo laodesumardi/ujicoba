19a8f3a8c2919ad5783d2961ab33b2c9aeded5bc
laodesumardi <sumardilaode04@gmail.com>
2025-10-25 08:06:33 +0900

    feat: Remove hardcoded address from contact page

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -201,15 +201,6 @@
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"></path>
                         </svg>
                     </a>
-                    <div class="mt-6 p-4 bg-white rounded-lg border border-blue-200">
-                        <p class="text-sm text-gray-600 flex items-center justify-center">
-                            <svg class="w-4 h-4 mr-2 text-blue-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path>
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path>
-                            </svg>
-                            <span class="font-medium">Alamat:</span> Jl. Pendidikan No. 123, Namrole, Maluku Tengah
-                        </p>
-                    </div>
                 </div>
             </div>
         </div>
