0938370323185ea372d17ad960a19d17eb118213
laodesumardi <sumardilaode04@gmail.com>
2025-10-25 01:43:15 +0900

    chore(fonts): replace Bunny Fonts Figtree with Google Fonts and add preconnect

--- resources/views/layouts/student.blade.php
+++ resources/views/layouts/student.blade.php
@@ -12,8 +12,9 @@
     <title>{{ config('app.name', 'SMP Negeri 01 Namrole') }} - @yield('title', 'Student Dashboard')</title>
 
     <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Figtree:wght@400;500;600&display=swap" rel="stylesheet" />
     
     <!-- FontAwesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
